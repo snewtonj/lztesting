7d080635ef421acc9acb42e902c91e12088bd098
Steven E. Newton <steven@janrain.com>
2013-01-03 10:03:32 -0800

    attempting to get a decent layout. Can you tell I'm not a UI programmer?

--- src/main/java/com/janrain/steven/lztesting/ui/LeaderElectorPanel.java
+++ src/main/java/com/janrain/steven/lztesting/ui/LeaderElectorPanel.java
@@ -17,63 +17,93 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 import org.apache.log4j.Logger;
 
-public class LeaderElectorPanel extends JPanel implements PropertyChangeListener {
-	
+public class LeaderElectorPanel extends JPanel implements
+PropertyChangeListener {
+
 	private JButton relinquishButton;
 	private JButton requeueButton;
 	private JLabel statusLabel;
 	private ImageIcon lightOn;
 	private ImageIcon lightOff;
 	private JLabel onOffLabel;
-	private static final Logger logger = Logger.getLogger(LeaderElectorPanel.class);
-	
+	private static final Logger logger = Logger
+			.getLogger(LeaderElectorPanel.class);
+
 	public LeaderElectorPanel() {
 		super();
-		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		SpringLayout layout = new SpringLayout();
+		this.setLayout(layout);
 		relinquishButton = new JButton("Relinquish");
 		relinquishButton.setEnabled(false);
-		relinquishButton.addActionListener(new ActionListener() {		
+		relinquishButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setLeader(false);
 			}
 		});
+		layout.putConstraint(SpringLayout.WEST, relinquishButton, 5,
+				SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.NORTH, relinquishButton, 5,
+				SpringLayout.NORTH, this);
 		add(relinquishButton);
+
 		requeueButton = new JButton("Requeue");
 		requeueButton.setEnabled(false);
-		requeueButton.addActionListener(new ActionListener() {			
+		requeueButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent evt) {
 				handleRequeue();
 			}
 		});
+		layout.putConstraint(SpringLayout.WEST, requeueButton, 5,
+				SpringLayout.EAST, relinquishButton);
+		layout.putConstraint(SpringLayout.NORTH, requeueButton, 5,
+				SpringLayout.NORTH, this);
 		add(requeueButton);
+
 		statusLabel = new JLabel("Ready");
 		loadImages(statusLabel.getSize());
+		layout.putConstraint(SpringLayout.WEST, statusLabel, 5,
+				SpringLayout.EAST, requeueButton);
+		layout.putConstraint(SpringLayout.NORTH, statusLabel, 5,
+				SpringLayout.NORTH, this);
 		add(statusLabel);
 		onOffLabel = new JLabel(lightOff);
+		layout.putConstraint(SpringLayout.WEST, onOffLabel, 5,
+				SpringLayout.EAST, statusLabel);
+		layout.putConstraint(SpringLayout.NORTH, onOffLabel, 5,
+				SpringLayout.NORTH, this);
+
+		layout.putConstraint(SpringLayout.EAST, this, 5, SpringLayout.EAST,
+				onOffLabel);
+		layout.putConstraint(SpringLayout.SOUTH, this, 5, SpringLayout.SOUTH,
+				onOffLabel);
 		add(onOffLabel);
 	}
 
 	private void loadImages(Dimension dimension) {
 		int size = 32;
 		lightOn = resizeImage(size, "slim-green-led-on-th.png");
- 		lightOff = resizeImage(size, "slim-green-led-off-th.png");
+		lightOff = resizeImage(size, "slim-green-led-off-th.png");
 	}
 
 	private ImageIcon resizeImage(int size, String imageURL) {
 		try {
 			BufferedImage sourceImage;
 			sourceImage = ImageIO.read(getClass().getResource(imageURL));
-			BufferedImage resizedImg = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-	        Graphics2D g2 = resizedImg.createGraphics();
-	        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	        g2.drawImage(sourceImage, 0, 0, size, size, null);
-	        g2.dispose();
-	        return new ImageIcon(resizedImg);
+			BufferedImage resizedImg = new BufferedImage(size, size,
+					BufferedImage.TYPE_INT_RGB);
+			Graphics2D g2 = resizedImg.createGraphics();
+			g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+					RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g2.drawImage(sourceImage, 0, 0, size, size, null);
+			g2.dispose();
+			return new ImageIcon(resizedImg);
 		} catch (IOException e) {
-			return new ImageIcon(getClass().getResource("slim-green-led-off-th.png"));
+			return new ImageIcon(getClass().getResource(
+					"slim-green-led-off-th.png"));
 		}
 	}
 
@@ -90,7 +120,7 @@ public class LeaderElectorPanel extends JPanel implements PropertyChangeListener
 	private static final long serialVersionUID = 1L;
 
 	public void setLeader(boolean isLeader) {
-		logger.debug("setLeader: "+isLeader);
+		logger.debug("setLeader: " + isLeader);
 		firePropertyChange("processing", !isLeader, isLeader);
 		requeueButton.setEnabled(!isLeader);
 		relinquishButton.setEnabled(isLeader);
@@ -102,22 +132,22 @@ public class LeaderElectorPanel extends JPanel implements PropertyChangeListener
 			statusLabel.setText("Not Queued");
 		}
 	}
-	
+
 	public void propertyChange(PropertyChangeEvent evt) {
-		logger.debug("Got property change event "+evt.getPropertyName());
+		logger.debug("Got property change event " + evt.getPropertyName());
 		String propertyName = evt.getPropertyName();
 
-		if("isLeader".equals(propertyName)) {
-			relinquishButton.setEnabled((Boolean)evt.getNewValue());
+		if ("isLeader".equals(propertyName)) {
+			relinquishButton.setEnabled((Boolean) evt.getNewValue());
 			statusLabel.setText(evt.getNewValue().toString());
 		} else if ("isQueued".equals(propertyName)) {
-			requeueButton.setEnabled(!(Boolean)evt.getNewValue());
+			requeueButton.setEnabled(!(Boolean) evt.getNewValue());
 		}
-		
+
 	}
 
 	public void setState(String name) {
-		statusLabel.setText(name);		
+		statusLabel.setText(name);
 	}
 
 }
